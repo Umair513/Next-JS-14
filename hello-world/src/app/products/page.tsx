666a39abe395800f23b5e77ca232412a160e9375
Umair513 <soomroumair513@gmail.com>
2023-11-28 23:29:25 +0500

    Link Component Navigation

--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
+  const productID = 100;
   return (
     <div>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/products/${productID}`}>Product {productID}</Link>
+      </h2>
+
+      <br></br>
+      <Link href="/">Home</Link>
     </div>
   );
 };
